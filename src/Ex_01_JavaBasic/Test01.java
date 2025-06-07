a4b7faf9b9756c7fc215ebb490a262c9f445e398
Satywans <skrider003@gmail.com>
2025-06-07 16:24:22 +0530

    new file

---
+++ src/Ex_01_JavaBasic/Test01.java
@@ -0,0 +1,11 @@
+package Ex_01_JavaBasic;
+
+public class Test01 {
+    public static void main(String[] args) {
+
+        int a=12;
+        int b=30;
+
+        System.out.println("sum of a and b = " + (a+b));
+    }
+}
